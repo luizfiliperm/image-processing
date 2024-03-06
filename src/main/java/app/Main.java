cea338af895588479eeda59fc554c576b5ff0dd9
luizfiliperm <luizfilipemr17@gmail.com>
2024-03-06 15:35:48 -0300

    Displaying Height, Width and pixels from an image on screen, and saving a copy of it

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,27 @@
 package app;
 
+import entities.Image;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Insert the path of the image: ");
+        String path = sc.nextLine();
+
+        Image image = new Image(path);
+
+        System.out.println("Height: " + image.getHeight());
+        System.out.println("Width: " + image.getWidth());
+        System.out.println("Pixels: " + image.getPixels());
 
 
+        String savePath = path.substring(0, path.lastIndexOf(".")) + "_copy.jpg";
+        image.saveImage(savePath);
+
+        sc.close();
     }
 }
