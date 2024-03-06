5075e8b3052352198ffa708c00a7048e600bb6f8
luizfiliperm <luizfilipemr17@gmail.com>
2024-03-06 17:31:15 -0300

    Showing RGB of every pixel

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -26,6 +26,7 @@ public class Main {
         ImageService.setCentralPixelGreen(image);
         ImageService.setLastPixelRed(image);
 
+        ImageService.showRGBofEveryPixel(image);
 
         image.saveImage(savePath);
 
--- src/main/java/services/ImageService.java
+++ src/main/java/services/ImageService.java
@@ -17,4 +17,14 @@ public class ImageService {
     public static void setLastPixelRed(Image image){
         image.getBufferedImage().setRGB(image.getWidth()-1, image.getHeight()-1, Color.RED.getRGB());
     }
+
+    public static void showRGBofEveryPixel(Image image){
+        // Format: x,y - R G B
+        for(int i = 0; i < image.getWidth(); i++){
+            for(int j = 0; j < image.getHeight(); j++){
+                Color color = new Color(image.getBufferedImage().getRGB(i,j));
+                System.out.println(i + "," + j + " - " + color.getRed() + " " + color.getGreen() + " " + color.getBlue());
+            }
+        }
+    }
 }
